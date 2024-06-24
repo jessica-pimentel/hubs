3023905c2be4fb6fa101780c05279ef9cfa4394a
jessica-pimentel <jppimentel96@hotmail.com>
2024-06-24 14:11:45 -0300

    first commit

---
+++ ChatSignalClient/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSignalClient
+{
+    internal class Class1
+    {
+    }
+}
--- ChatSignalClient/Program.cs
+++ ChatSignalClient/Program.cs
@@ -1,17 +1,33 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 const string url = "https://localhost:7281/hub";
 
-await using var connection = new HubConnectionBuilder()
+var connection = new HubConnectionBuilder()
     .WithUrl(url)
+    .ConfigureLogging(logging =>
+    {
+        logging.AddConsole();
+        logging.SetMinimumLevel(LogLevel.Information);
+    })
     .Build();
 
+connection.On<DateTime>("ReceiveDateTime", (date) =>
+{
+    Console.WriteLine($"Received DateTime: {date}");
+});
 
 await connection.StartAsync();
-
+Console.WriteLine("Connection started. Listening for DateTime updates...");
 
 await foreach (var date in connection.StreamAsync<DateTime>("Streaming"))
 {
-    Console.WriteLine(date);
+    Console.WriteLine($"Received DateTime: {date}");
 }
 
+// Manter o programa em execução para continuar recebendo mensagens
+await Task.Delay(Timeout.Infinite);
+
+//teste
